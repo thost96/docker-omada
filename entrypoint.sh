6a2a855c0182f84985502060cb025ebfcd6c98be
Thorsten Reichelt <info@thorstenreichelt.de>
2021-01-16 20:21:37 +0100

    pre-2.1.0

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 rm -f /opt/tplink/EAPController/data/db/journal/prealloc.*
+
+sed -i 's/http.connector.port=8088/http.connector.port='"${HTTPPORT}"'/g' /opt/tplink/EAPController/properties/jetty.properties \
+&& sed -i 's/https.connector.port=8043/https.connector.port='"${HTTPSPORT}"'/g' /opt/tplink/EAPController/properties/jetty.properties
+
 exec /opt/tplink/EAPController/jre/bin/java \
        -server -Xms128m -Xmx1024m -XX:MaxHeapFreeRatio=60 -XX:MinHeapFreeRatio=30 \
        -Deap.home=/opt/tplink/EAPController \
